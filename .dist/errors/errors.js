9ee590cf7876691ff425304fb824e76593f1b75c
nabbas <nasir000376@gmail.com>
2023-07-28 18:19:10 +0200

    :recycle: Add .dist folder to .gitignore

--- .dist/errors/errors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorResult = void 0;
-class ErrorResult extends Error {
-    constructor(message) {
-        super(message);
-    }
-}
-exports.ErrorResult = ErrorResult;
